ef18edb658421284416f5d83913c54e5af772427
Cristian Lariccia <cristil.mail@gmail.com>
2021-03-22 22:37:02 -0300

    Adicionado PAGE OBJECTS e FEATURE

--- src/test/java/pages/List.java
+++ src/test/java/pages/List.java
@@ -1,4 +1,32 @@
 package pages;
 
-public class List {
+import general.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class List extends Base {
+
+    //1- mapeamento
+    @FindBy (css =  "h1.h2Categoria.nomeCategoria")
+    private WebElement lblResultBy;
+
+
+    //2 - construtor
+    public List(WebDriver driver) {
+        super(driver);
+    }
+
+    //3 - métodos
+    public String ReadTitleTab(){
+        return driver.getTitle();
+    }
+    public void clickAtDesiredProduct(String product){
+        driver.findElement(By.xpath("//h3[contains(text(),['" + product + "'])]")).click();
+    }
+
+    public String readResultBy(){
+        return lblResultBy.getText();
+    }
 }
